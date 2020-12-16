6b38fb62f825d6c1726c5bad49dfd88cc038107d
jijinshan <jijinshan@163.com>
2020-12-16 12:05:34 +0800

    美多商城项目-mysql数据库配置

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '192.168.19.131',
+        'PORT': 3306,
+        'USER': 'itcast',
+        'PASSWORD': '123456abc',
+        'NAME': 'meiduo',
     }
 }
 
