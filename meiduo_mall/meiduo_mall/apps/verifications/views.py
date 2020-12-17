34909f338d5bae385f19353fc54baa860ebb4165
jijinshan <jijinshan@163.com>
2020-12-17 15:01:15 +0800

    美多商城项目-手机验证码发送

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,15 +1,60 @@
-from django.http import HttpResponse
+import random
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
+from django_redis import get_redis_connection
 from meiduo_mall.libs.captcha.captcha import captcha
 # Create your views here.
 
+import logging
+
+from meiduo_mall.libs.yuntongxun.ccp_sms import CCP
+
+logger = logging.getLogger("django")
+
 
 # GET /image_codes/(?P<uuid>[\w-]+)/
-
-
-
 class ImageCodeView(View):
     def get(self, request, uuid):
         text, image = captcha.generate_captcha()
+        redis_conn = get_redis_connection("verify_code")
+        redis_conn.set("img_%s" % uuid, text, 300)
         return HttpResponse(image, content_type="image/jpeg")
+
+
+# GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
+class SMSCodeView(View):
+    def get(self, request, mobile):
+        redis_conn = get_redis_connection("verify_code")
+        send_flag = redis_conn.get("send_flag_%s" % mobile)
+        if send_flag:
+            return JsonResponse({"code": 400,
+                                 "message": "获取验证码频繁"})
+        image_code = request.GET.get("image_code")
+        uuid = request.GET.get("image_code_id")
+        if not all([image_code, uuid]):
+            return JsonResponse({"code": 400,
+                                 "message": "缺少必传参数"})
+        image_code_redis = redis_conn.get("img_%s" % uuid)
+        if image_code_redis is None:
+            return JsonResponse({"code": 400,
+                                 "message": "图形验证码失效"})
+        try:
+            redis_conn.delete("img_%s" % uuid)
+        except Exception as e:
+            logger.error(e)
+        if image_code.lower() != image_code_redis.lower():
+            return JsonResponse({"code": 400,
+                                 "message": "验证码输入错误"})
+        sms_code = "%06d" % random.randint(0, 999999)
+        logger.info("短信验证码为: %s" % sms_code)
+        # redis_conn.set("sms_%s" % mobile, sms_code, 300)
+        # redis_conn.set("send_flag_%s" % mobile, 1, 60)
+        pl = redis_conn.pipeline()
+        pl.set("sms_%s" % mobile, sms_code, 300)
+        pl.set("send_flag_%s" % mobile, 1, 60)
+        pl.execute()
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        return JsonResponse({"code": 0,
+                             "message": "发送短信成功"})
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -147,6 +147,18 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     },
+    "verify_code": {
+        # 验证码信息: 存到 2 号库
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://192.168.19.131:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # 从redis中查询出数据之后，会自动将bytes数据解码为str
+            "CONNECTION_POOL_KWARGS": {
+                'decode_responses': True
+            }
+        }
+    },
 }
 
 # 设置session数据存储到CACHES缓存中
