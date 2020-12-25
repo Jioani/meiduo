e96215cf8eca58d4322ea6f92924e1c67d95049b
jijinshan <jijinshan@163.com>
2020-12-25 20:35:21 +0800

    美多商城项目-商品详情页实现

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -5,5 +5,4 @@ from goods import views
 urlpatterns = [
     re_path(r"^list/(?P<category_id>\d+)/skus/$", views.SKUListView.as_view()),
     re_path(r"^hot/(?P<category_id>\d+)/$", views.SKUHotView.as_view()),
-    re_path(r"^search/$", views.SKUSearchView.as_view()),
 ]
